84684d6fc7ca72b4ab58f46d7ef20089af148174
CJ Chen <sijia1996@gmail.com>
2026-04-04 17:35:19 -0700

    Clean up code

--- financesync/commands.py
+++ financesync/commands.py
@@ -3,41 +3,24 @@ from financesync.google_api.gmail import get_citi_transactions, get_transactions
 from financesync.google_api.sheets import write_transactions
 from financesync.utils.config import SPREADSHEET_ID
 from financesync.google_api.credentials import authenticate, remove_credentials
-def sync():
-
-  transactions = None
-  message_ids = None
-  
-  transactions, message_ids = get_transactions() 
-
-  print("=======================")
-  print("Chase")
-  print("=======================")
-
-  pp(transactions)
-
-  write_transactions(transactions, SPREADSHEET_ID)
-
-  unlabel(message_ids)
-
-  transactions = None
-  message_ids = None
-  
-  transactions, message_ids = get_citi_transactions() 
-  print("=======================")
-  print("Citi")
-  print("=======================")
-
-  pp(transactions)
-
-  write_transactions(transactions, SPREADSHEET_ID)
-
-  unlabel(message_ids)
-
 import webbrowser
-from financesync.utils.config import SPREADSHEET_ID
 
-# This doesn't work currently
+def sync():
+  
+  chase_transactions, message_ids = get_transactions() 
+  
+  citi_transactions, message_ids = get_citi_transactions() 
+
+  transactions = chase_transactions + citi_transactions
+
+  transactions.sort(key=lambda x: x.date)
+
+  write_transactions(transactions, SPREADSHEET_ID)
+
+  unlabel(message_ids)
+
+  pp(transactions)
+
 def open():
     webbrowser.open_new_tab(f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}")
 
